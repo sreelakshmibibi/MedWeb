955513809fb6e7812903c8f85ebefecbcb337624
rintu-reji <rintucheruvathoor@gmail.com>
2024-06-13 13:50:29 +0530

    updated footer
    updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 @section('footer')
-    <div class="container position-fixed bottom-0" style="left:0;
+    <div class="container-fluid position-fixed bottom-0" style="left:0;
     right:0;">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-1 border-top">
             <div class="col-md-4 d-flex align-items-center">
