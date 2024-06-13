ee79ac3469d9cc6eaae6691f4e53a5a0823562dd
shincydelbin <shincydelbin2023@gmail.com>
2024-06-13 17:28:39 +0530

    Patient profile model and migration
    This commit includes Patient profile model and migration

---
+++ database/migrations/2024_06_13_110756_create_patient_profiles_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_profiles', function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger('patient_id')->unique(); 
+            $table->string('national_id', 50)->nullable(); 
+            $table->string('first_name', 100); 
+            $table->string('last_name', 100)->nullable(); 
+            $table->string('gsm', 50)->nullable(); 
+            $table->string('gender', 20)->nullable(); 
+            $table->date('birth_date')->nullable(); 
+            $table->integer('age')->nullable(); 
+            $table->integer('area')->nullable(); 
+            $table->foreignId('nationality')->constrained('countries'); 
+            $table->string('registration_date', 50)->nullable(); 
+            $table->foreignId('doctor_id')->constrained('doctor_profiles'); 
+            $table->integer('visit_count')->default(0); 
+            $table->string('pstatus', 1)->default('Y'); 
+            $table->string('regby', 50);
+            
+            
+            $table->index('first_name'); 
+            $table->index('last_name'); 
+            $table->index('gsm'); 
+            $table->index('area');
+            $table->index('nationality'); 
+            $table->index('visit_count'); 
+            $table->index('age'); 
+            $table->index('birth_date'); 
+            $table->index('gender'); 
+            $table->index('national_id'); 
+            $table->index('regby');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_profiles');
+    }
+};
