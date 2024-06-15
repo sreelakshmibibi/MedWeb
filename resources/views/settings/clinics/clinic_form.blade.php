edfb49850db9da51bb24053134c5b6074ba476fa
rintu-reji <rintucheruvathoor@gmail.com>
2024-06-15 13:11:39 +0530

    clinic form updated

--- resources/views/settings/clinics/clinic_form.blade.php
+++ resources/views/settings/clinics/clinic_form.blade.php
@@ -3,12 +3,18 @@
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <div class="container-full">
+            <!-- alert -->
+            <div class="myadmin-alert myadmin-alert-icon myadmin-alert-click alert-success alerttop fadeOut"
+                style="display: none;">
+                <i class="ti-check"></i> New clinic branch added <a href="#" class="closed">×</a>
+            </div>
+
             <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="d-flex align-items-center justify-content-between">
                     <h3 class="page-title">Clinic Details</h3>
-                    <button type="button" class="waves-effect waves-light btn btn-primary mb-5" data-bs-toggle="modal"
-                        data-bs-target="#modal-center"> <i class="fa fa-add"></i> Add New</button>
+                    <button type="button" class="waves-effect waves-light btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#modal-right"> <i class="fa fa-add"></i> Add New</button>
                 </div>
             </div>
 
@@ -18,24 +24,24 @@
                     <div class="box">
                         <div class="box-body">
                             <div class="table-responsive">
-                        <!-- Main content -->
+                                <!-- Main content -->
                                 <table class="table table-bordered table-hover table-striped mb-0 border-2 data-table">
                                     <thead class="bg-primary-light">
                                         <tr>
-                                        <th>No</th>
-                                        <th>Name</th>
-                                        <th>Phone Number</th>
-                                        <th>Address</th>
-                                        <th width="100px">Action</th>
+                                            <th>No</th>
+                                            <th>Name</th>
+                                            <th>Phone Number</th>
+                                            <th>Address</th>
+                                            <th width="100px">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
+
                                     </tbody>
                                 </table>
-                        <!-- /.content -->
+                                <!-- /.content -->
                             </div>
-                   
+
                         </div>
                         <!-- /.box -->
                     </div>
@@ -49,23 +55,39 @@
 
     <!-- modal -->
     @include('settings.clinics.create')
+
     <script type="text/javascript">
-        
-        jQuery(function($){
-        
+        jQuery(function($) {
+
             var table = $('.data-table').DataTable({
                 processing: true,
                 serverSide: true,
                 ajax: "{{ route('settings.clinic') }}",
-                columns: [
-                    {data: 'id', name: 'id'},
-                    {data: 'clinic_name', name: 'clinic_name'},
-                    {data: 'phone_number', name: 'phone_number'},
-                    {data: 'clinic_address', name: 'clinic_address'},
-                    {data: 'action', name: 'action', orderable: false, searchable: true},
+                columns: [{
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'clinic_name',
+                        name: 'clinic_name'
+                    },
+                    {
+                        data: 'phone_number',
+                        name: 'phone_number'
+                    },
+                    {
+                        data: 'clinic_address',
+                        name: 'clinic_address'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: true
+                    },
                 ]
             });
-                
+
         });
     </script>
 @endsection
