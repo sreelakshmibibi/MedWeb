79ee653febf36c5cee079ab291a445402e246777
rintu-reji <rintucheruvathoor@gmail.com>
2024-06-17 19:30:27 +0530

    delete blade

--- resources/views/settings/treatment_cost/delete.blade.php
+++ resources/views/settings/treatment_cost/delete.blade.php
@@ -1,14 +1,14 @@
-<!-- delete modal-->
-<div class="modal fade" id="modal-delete" role="dialog">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Delete</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form id="form-delete" method="POST">
-                @csrf
-                @method('DELETE')
+<form id="form-delete" method="POST">
+    @csrf
+    @method('DELETE')
+    <!-- delete modal-->
+    <div class="modal fade" id="modal-delete" role="dialog">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Delete</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
                 <div class="modal-body">
                     <p>Are you sure you want to delete the treatment?</p>
                     <input type="hidden" id="delete_treatment_id" name="delete_treatment_id" value="">
@@ -18,9 +18,9 @@
                     <button type="button" class="btn btn-danger float-end" data-bs-dismiss="modal"
                         id="btn-confirm-delete">Delete</button>
                 </div>
-            </form>
+            </div>
+            <!-- /.modal-content -->
         </div>
-        <!-- /.modal-content -->
+        <!-- /.modal-dialog -->
     </div>
-    <!-- /.modal-dialog -->
-</div>
+</form>
