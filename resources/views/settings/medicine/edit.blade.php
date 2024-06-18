525ff970db1cae973574bd7916e6f1f0a62c8d98
rintu-reji <rintucheruvathoor@gmail.com>
2024-06-18 10:29:31 +0530

    medicine blade

---
+++ resources/views/settings/medicine/edit.blade.php
@@ -0,0 +1,140 @@
+<form method="post" action="{{ route('settings.medicine.update') }}">
+    @csrf
+    <input type="hidden" id="edit_medicine_id" name="edit_medicine_id" value="">
+    <div class="modal modal-right slideInRight" id="modal-edit" tabindex="-1">
+        <div class="modal-dialog" style="width:40%; max-width: 80%;">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"><i class="fa fa-briefcase"> </i> Edit Medicine Details</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="container-fluid">
+
+                        <div class="form-group">
+                            <label class="form-label" for="name">Medicine Name</label>
+                            <input class="form-control" type="text" id="name" name="name" required
+                                minlength="3" placeholder="Medicine Name">
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="barcode">Barcode</label>
+                                    <input class="form-control" type="text" id="barcode" name="barcode" required
+                                        minlength="3" placeholder="Barcode">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="strength">Strength</label>
+                                    <input class="form-control" type="text" id="strength" name="strength" required
+                                        minlength="3" placeholder="Medicine Strength">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-label" for="strength">Strength</label>
+                            <input class="form-control" type="text" id="strength" name="strength" required
+                                minlength="3" placeholder="Medicine Strength">
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-label" for="remarks">Remarks</label>
+                            <input class="form-control" type="text" id="remarks" name="remarks" required
+                                minlength="3" placeholder="Medicine Remarks">
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="price">Price</label>
+                                    <input class="form-control" type="text" id="price" name="price" required
+                                        minlength="3" placeholder="Medicine Price">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="mstatus">Medicine Status</label>
+                                    <input class="form-control" type="text" id="mstatus" name="mstatus" required
+                                        minlength="3" placeholder="Medicine Status">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group mt-2">
+                            <label class="form-label col-md-6">Status</label>
+                            <input name="status" type="radio" checked class="form-control with-gap" id="yes"
+                                value="Y">
+                            <label for="yes">Yes</label>
+                            <input name="status" type="radio" class="form-control with-gap" id="no"
+                                value="N">
+                            <label for="no">No</label>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="mdate">Medicine Date</label>
+                                    <input class="form-control" type="date" id="mdate" name="mdate"
+                                        required minlength="3" placeholder="Medicine Date">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label" for="mlast_update">Expiring Date</label>
+                                    <input class="form-control" type="date" id="mlast_update" name="mlast_update"
+                                        required minlength="3" placeholder="Expiring date">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-label" for="mname">Medicine Name</label>
+                            <input class="form-control" type="text" id="mname" name="mname" required
+                                minlength="3" placeholder="Medicine Name">
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-label" for="strength">Strength</label>
+                            <input class="form-control" type="text" id="strength" name="strength" required
+                                minlength="3" placeholder="Medicine Strength">
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-8">
+                                <label class="form-label" for="name">Barcode</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="barcodeInput"
+                                        placeholder="Enter text...">
+                                    {{-- <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload"> --}}
+                                    <button class="btn btn-primary btn-sm input-group-text" type="button"
+                                        id="inputGroupFileAddon04" onclick="generateBarcode()">Generate
+                                        Barcode</button>
+                                </div>
+                            </div>
+
+                            <canvas id="barcodeCanvas" class="col-md-4" style=" height:64px;"></canvas>
+                        </div>
+
+                        <div class="form-group mt-2">
+                            <label class="form-label col-md-6">Active</label>
+                            <input name="status" type="radio" checked class="form-control with-gap"
+                                id="yes" value="Y">
+                            <label for="yes">Yes</label>
+                            <input name="status" type="radio" class="form-control with-gap" id="no"
+                                value="N">
+                            <label for="no">No</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer modal-footer-uniform">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success float-end" id="buttonalert">Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
---
+++ resources/views/settings/medicine/index.blade.php
@@ -0,0 +1,188 @@
+@extends('layouts.dashboard')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <div class="container-full">
+            <div class="content-header">
+                @if (session('success'))
+                    <div class="myadmin-alert myadmin-alert-icon myadmin-alert-click alert-success alerttop fadeOut"
+                        style="display: block;">
+                        <i class="ti-check"></i> {{ session('success') }} <a href="#" class="closed">×</a>
+                    </div>
+                @endif
+                @if (session('error'))
+                    <div class="myadmin-alert myadmin-alert-icon myadmin-alert-click alert-danger alerttop fade fadeOut"
+                        style="display: block;">
+                        <i class="ti-check"></i> {{ session('error') }} <a href="#" class="closed">×</a>
+                    </div>
+                @endif
+                <div class="d-flex align-items-center justify-content-between">
+                    <h3 class="page-title">Medicine Details</h3>
+                    <button type="button" class="waves-effect waves-light btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#modal-right"> <i class="fa fa-add"></i> Add New</button>
+                </div>
+            </div>
+
+            <section class="content">
+                <div class="box">
+                    {{-- <div class="box-body p-0"> --}}
+                    <div class="box-body">
+                        <div class="table-responsive">
+                            <!-- Main content -->
+                            <table
+                                class="table table-bordered table-hover table-striped mb-0 border-2 data-table text-center">
+                                <thead class="bg-primary-light">
+                                    <tr>
+                                        <th>No</th>
+                                        <th>med_bar_code</th>
+                                        <th>Name</th>
+                                        <th>Strength</th>
+                                        <th>remarks</th>
+                                        <th>price</th>
+                                        <th>med_status</th>
+                                        <th>Status</th>
+                                        <th>med_date</th>
+                                        <th>med_last_update</th>
+                                        <th>med_name</th>
+                                        <th>med_strength</th>
+                                        <th>Status</th>
+                                        <th width="100px">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                            <!-- /.content -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <!-- /.content-wrapper -->
+
+    @include('settings.medicine.create')
+    @include('settings.medicine.edit')
+    @include('settings.medicine.delete')
+    {{-- </div> --}}
+
+    <!-- ./wrapper -->
+    {{-- <script type="module"> --}}
+    <script type="text/javascript">
+        jQuery(function($) {
+
+            var table = $('.data-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('settings.medicine') }}",
+                columns: [{
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'department',
+                        name: 'department'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: true
+                    },
+                ]
+            });
+            $(document).on('click', '.btn-edit', function() {
+                var departmentId = $(this).data('id');
+                $('#edit_department_id').val(departmentId); // Set department ID in the hidden input
+                $.ajax({
+                    url: '{{ url('department', '') }}' + "/" + departmentId + "/edit",
+                    method: 'GET',
+                    success: function(response) {
+                        $('#edit_department_id').val(response.id);
+                        $('#edit_department').val(response.department);
+
+                        if (response.status === 'Y') {
+                            $('#edit_yes').prop('checked', true);
+                        } else {
+                            $('#edit_no').prop('checked', true);
+                        }
+
+                        $('#modal-edit').modal('show');
+                    },
+                    error: function(error) {
+                        console.log(error)
+                    }
+                });
+
+            });
+            $(document).on('click', '.btn-danger', function() {
+                var departmentId = $(this).data('id');
+                $('#delete_department_id').val(departmentId); // Set department ID in the hidden input
+                $('#modal-delete').modal('show');
+            });
+
+            $('#btn-confirm-delete').click(function() {
+                var departmentId = $('#delete_department_id').val();
+                var url = "{{ route('settings.departments.destroy', ':department') }}";
+                url = url.replace(':department', departmentId);
+
+                $.ajax({
+                    type: 'DELETE',
+                    url: url,
+                    data: {
+                        "_token": "{{ csrf_token() }}"
+                    },
+                    success: function(response) {
+                        table.draw();
+
+                    },
+                    error: function(xhr) {
+                        $('#modal-delete').modal('hide');
+                        swal("Error!", xhr.responseJSON.message, "error");
+                    }
+                });
+            });
+
+        });
+
+        // barcode
+        function generateBarcode() {
+            // Get input value
+            var inputValue = document.getElementById("barcodeInput").value.trim();
+
+            // Check if input value is empty
+            if (inputValue === "") {
+                alert("Please enter text to generate barcode.");
+                return;
+            }
+
+            // Generate barcode
+            JsBarcode("#barcodeCanvas", inputValue, {
+                format: "CODE128", // Barcode format (you can choose other formats like EAN-13, QR Code, etc.)
+                displayValue: true, // Display value below barcode
+                fontSize: 16,
+                textMargin: 10,
+                width: 2,
+                height: 50
+            });
+
+            JsBarcode("#barcodeCanvas", inputValue, {
+                format: "CODE128", // Barcode format (CODE128, EAN-13, etc.)
+                displayValue: true, // Show human-readable value below barcode
+                fontSize: 16, // Font size of the value text
+                textMargin: 10, // Margin between barcode and value text
+                width: 2, // Barcode bar width (in pixels)
+                height: 50, // Barcode height (in pixels)
+                margin: 10, // Margin around the barcode (in pixels)
+                background: "#f0f0f0", // Background color of the barcode
+                // height: 25,
+                lineColor: "#333" // Color of the barcode bars
+            });
+        }
+    </script>
+@endsection
