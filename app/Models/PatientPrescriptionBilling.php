3217febb30e67d2ce14780d69efbc82129ea4e14
Sreelakshmi M S <sreelakshmi.ms@gmail.com>
2024-08-02 10:17:58 +0530

    Billing migrations
    Billing migrations

---
+++ app/Models/PatientPrescriptionBilling.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PatientPrescriptionBilling extends Model
+{
+    use HasFactory;
+}
