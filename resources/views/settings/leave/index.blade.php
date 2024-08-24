d23e30a5fb891947aa914c1656ca3801a8f6bf38
rintu-reji <rintucheruvathoor@gmail.com>
2024-08-24 16:06:21 +0530

    leave blades updated

--- resources/views/settings/leave/index.blade.php
+++ resources/views/settings/leave/index.blade.php
@@ -23,7 +23,7 @@
                     <h3 class="page-title">Leave Details</h3>
                     @if (Auth::user()->can('leave apply'))
                         <button type="button" class="waves-effect waves-light btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#modal-right"> <i class="fa fa-add"></i> Apply Leave</button>
+                            data-bs-target="#modal-right"> <i class="fa fa-add"></i> Apply Leave</button>
                     @endif
                 </div>
             </div>
@@ -38,13 +38,13 @@
                                     <tr>
                                         <th width="10px">No</th>
                                         @if (Auth::user()->can('leave approve'))
-                                            <th width="20%">Staff</th>
+                                            <th>Staff</th>
                                         @endif
-                                        <th width="15%">Leave Type</th>
+                                        <th width="10%">Leave Type</th>
                                         <th width="25%">Dates (Days)</th>
                                         <th width="30%">Reason</th>
                                         <th width="20px">Status</th>
-                                        <th width="80px">Action</th>
+                                        <th width="200px">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -67,169 +67,174 @@
 
 
     <script type="text/javascript">
-    var today = new Date().toISOString().split('T')[0];
-    document.getElementById('leave_from').setAttribute('min', today);
-    document.getElementById('leave_to').setAttribute('min', today);
-    document.getElementById('editleave_from').setAttribute('min', today);
-    document.getElementById('editleave_to').setAttribute('min', today);
+        var today = new Date().toISOString().split('T')[0];
+        document.getElementById('leave_from').setAttribute('min', today);
+        document.getElementById('leave_to').setAttribute('min', today);
+        document.getElementById('editleave_from').setAttribute('min', today);
+        document.getElementById('editleave_to').setAttribute('min', today);
 
-    jQuery(function($) {
-        var table = $(".data-table").DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: "{{ route('leave') }}",
-            columns: [
-                {
-                    data: "DT_RowIndex",
-                    name: "DT_RowIndex",
-                    orderable: false,
-                    searchable: false,
-                    render: function(data, type, row, meta) {
-                        return meta.row + 1;
+        jQuery(function($) {
+            var table = $(".data-table").DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('leave') }}",
+                columns: [{
+                        data: "DT_RowIndex",
+                        name: "DT_RowIndex",
+                        orderable: false,
+                        searchable: false,
+                        render: function(data, type, row, meta) {
+                            return meta.row + 1;
+                        },
                     },
-                },
-                @can('leave approve')
-                {
-                    data: "staff",
-                    name: "staff",
-                    className: "text-left",
-                },
-                @endcan
-                {
-                    data: "leave_type",
-                    name: "leave_type",
-                    className: "text-left",
-                },
-                {
-                    data: "leave_applied_dates",
-                    name: "leave_applied_dates",
-                },
-                {
-                    data: "leave_reason",
-                    name: "leave_reason",
-                    className: "text-left",
-                },
-                {
-                    data: "status",
-                    name: "status",
-                    className: "text-center",
-                },
-                {
-                    data: "action",
-                    name: "action",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: true,
-                },
-            ],
-        });
+                    @can('leave approve')
+                        {
+                            data: "staff",
+                            name: "staff",
+                            className: "text-left",
+                        },
+                    @endcan {
+                        data: "leave_type",
+                        name: "leave_type",
+                        className: "text-left",
+                    },
+                    {
+                        data: "leave_applied_dates",
+                        name: "leave_applied_dates",
+                    },
+                    {
+                        data: "leave_reason",
+                        name: "leave_reason",
+                        className: "text-left",
+                    },
+                    {
+                        data: "status",
+                        name: "status",
+                        className: "text-center",
+                    },
+                    {
+                        data: "action",
+                        name: "action",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: true,
+                    },
+                ],
+            });
 
-        // Edit Leave
-        $(document).on('click', '.btn-edit', function() {
-            var leaveId = $(this).data('id');
-            $('#edit_leave_id').val(leaveId);
-            $.ajax({
-                url: '{{ url("leave") }}/' + leaveId + '/edit',
-                method: 'GET',
-                success: function(response) {
-                    $('#editleave_id').val(response.id);
-                    $('#editleave_type').val(response.leave_type);
-                    $('#editleave_from').val(response.leave_from);
-                    $('#editleave_to').val(response.leave_to);
-                    $('#editreason').val(response.leave_reason);
-                    $('#modal-edit').modal('show');
-                },
-                error: function(error) {
-                    console.log(error);
+            // Edit Leave
+            $(document).on('click', '.btn-edit', function() {
+                var leaveId = $(this).data('id');
+                $('#edit_leave_id').val(leaveId);
+                $.ajax({
+                    url: '{{ url('leave') }}/' + leaveId + '/edit',
+                    method: 'GET',
+                    success: function(response) {
+                        $('#editleave_id').val(response.id);
+                        $('#editleave_type').val(response.leave_type);
+                        $('#editleave_from').val(response.leave_from);
+                        $('#editleave_to').val(response.leave_to);
+                        $('#editreason').val(response.leave_reason);
+                        $('#modal-edit').modal('show');
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+            });
+
+            // Approve Leave
+            $(document).on('click', '.btn-approve', function() {
+                var leaveId = $(this).data('id');
+                $('#approve_leave_id').val(leaveId);
+                $('#modal-approve').modal('show');
+            });
+
+            $('#btn-confirm-approve').click(function() {
+                var leaveId = $('#approve_leave_id').val();
+                $.ajax({
+                    type: 'GET',
+                    url: "{{ route('leave.approve', ':leave') }}".replace(':leave', leaveId),
+                    data: {
+                        "_token": "{{ csrf_token() }}"
+                    },
+                    success: function(response) {
+                        $('#modal-approve').modal('hide');
+                        table.draw();
+                        $('#successMessage').text(response.success).fadeIn().delay(3000)
+                            .fadeOut();
+                    },
+                    error: function(xhr) {
+                        $('#modal-approve').modal('hide');
+                        console.log("Error:", xhr.responseJSON.message);
+                    }
+                });
+            });
+
+            // Reject Leave
+            $(document).on('click', '.btn-reject', function() {
+                var leaveId = $(this).data('id');
+                $('#reject_leave_id').val(leaveId);
+                $('#modal-reject').modal('show');
+            });
+
+            $('#btn-confirm-reject').click(function() {
+                var leaveId = $('#reject_leave_id').val();
+                var reason = $('#reject_reason').val();
+
+                if (reason.length === 0) {
+                    $('#reject_reason').addClass('is-invalid');
+                    $('#rejectionError').text('Reason is required.');
+                    return;
                 }
+
+                $.ajax({
+                    type: 'POST',
+                    url: "{{ route('leave.reject', ':leave') }}".replace(':leave', leaveId),
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        "reject_reason": reason
+                    },
+                    success: function(response) {
+                        table.draw();
+                        $('#modal-reject').modal('hide');
+                        $('#successMessage').text(response.success).fadeIn().delay(3000)
+                            .fadeOut();
+                    },
+                    error: function(xhr) {
+                        $('#modal-reject').modal('hide');
+                        console.log("Error:", xhr.responseJSON.message);
+                    }
+                });
+            });
+
+            // Delete Leave
+            $(document).on('click', '.btn-danger', function() {
+                var leaveId = $(this).data('id');
+                $('#delete_leave_id').val(leaveId);
+                $('#modal-delete').modal('show');
+            });
+
+            $('#btn-confirm-delete').click(function() {
+                var leaveId = $('#delete_leave_id').val();
+                $.ajax({
+                    type: 'DELETE',
+                    url: "{{ route('leave.destroy', ':leave') }}".replace(':leave', leaveId),
+                    data: {
+                        "_token": "{{ csrf_token() }}"
+                    },
+                    success: function(response) {
+                        table.draw();
+                        $('#successMessage').text('Leave application deleted successfully')
+                            .fadeIn().delay(3000).fadeOut();
+                    },
+                    error: function(xhr) {
+                        $('#modal-delete').modal('hide');
+                        console.log("Error:", xhr.responseJSON.message);
+                    }
+                });
             });
         });
-
-        // Approve Leave
-        $(document).on('click', '.btn-approve', function() {
-            var leaveId = $(this).data('id');
-            $('#approve_leave_id').val(leaveId);
-            $('#modal-approve').modal('show');
-        });
-
-        $('#btn-confirm-approve').click(function() {
-            var leaveId = $('#approve_leave_id').val();
-            $.ajax({
-                type: 'GET',
-                url: "{{ route('leave.approve', ':leave') }}".replace(':leave', leaveId),
-                data: { "_token": "{{ csrf_token() }}" },
-                success: function(response) {
-                    $('#modal-approve').modal('hide');
-                    table.draw();
-                    $('#successMessage').text(response.success).fadeIn().delay(3000).fadeOut();
-                },
-                error: function(xhr) {
-                    $('#modal-approve').modal('hide');
-                    console.log("Error:", xhr.responseJSON.message);
-                }
-            });
-        });
-
-        // Reject Leave
-        $(document).on('click', '.btn-reject', function() {
-            var leaveId = $(this).data('id');
-            $('#reject_leave_id').val(leaveId);
-            $('#modal-reject').modal('show');
-        });
-
-        $('#btn-confirm-reject').click(function() {
-            var leaveId = $('#reject_leave_id').val();
-            var reason = $('#reject_reason').val();
-
-            if (reason.length === 0) {
-                $('#reject_reason').addClass('is-invalid');
-                $('#rejectionError').text('Reason is required.');
-                return;
-            }
-
-            $.ajax({
-                type: 'POST',
-                url: "{{ route('leave.reject', ':leave') }}".replace(':leave', leaveId),
-                data: {
-                    "_token": "{{ csrf_token() }}",
-                    "reject_reason": reason
-                },
-                success: function(response) {
-                    table.draw();
-                    $('#modal-reject').modal('hide');
-                    $('#successMessage').text(response.success).fadeIn().delay(3000).fadeOut();
-                },
-                error: function(xhr) {
-                    $('#modal-reject').modal('hide');
-                    console.log("Error:", xhr.responseJSON.message);
-                }
-            });
-        });
-
-        // Delete Leave
-        $(document).on('click', '.btn-danger', function() {
-            var leaveId = $(this).data('id');
-            $('#delete_leave_id').val(leaveId);
-            $('#modal-delete').modal('show');
-        });
-
-        $('#btn-confirm-delete').click(function() {
-            var leaveId = $('#delete_leave_id').val();
-            $.ajax({
-                type: 'DELETE',
-                url: "{{ route('leave.destroy', ':leave') }}".replace(':leave', leaveId),
-                data: { "_token": "{{ csrf_token() }}" },
-                success: function(response) {
-                    table.draw();
-                    $('#successMessage').text('Leave application deleted successfully').fadeIn().delay(3000).fadeOut();
-                },
-                error: function(xhr) {
-                    $('#modal-delete').modal('hide');
-                    console.log("Error:", xhr.responseJSON.message);
-                }
-            });
-        });
-    });
-</script>
+    </script>
 
 @endsection
