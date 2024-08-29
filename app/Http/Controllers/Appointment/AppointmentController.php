7e9470ecfb789c45f4a07febff5c9614e9dba335
Sreelakshmi M S <sreelakshmi.ms@gmail.com>
2024-08-29 14:04:38 +0530

    updation for hiding treatment icon once bill is generated
    updation for hiding treatment icon once bill is generated

--- app/Http/Controllers/Appointment/AppointmentController.php
+++ app/Http/Controllers/Appointment/AppointmentController.php
@@ -8,6 +8,7 @@ use App\Models\AppointmentStatus;
 use App\Models\AppointmentType;
 use App\Models\ClinicBranch;
 use App\Models\DoctorWorkingHour;
+use App\Models\PatientTreatmentBilling;
 use App\Models\StaffProfile;
 use App\Services\CommonService;
 use App\Services\DoctorAvaialbilityService;
@@ -171,8 +172,13 @@ class AppointmentController extends Controller
                             // If appointment date is less than the selected date, show view icon
                             $buttons[] = "<a href='" . route('treatment', $idEncrypted) . "' class='waves-effect waves-light btn btn-circle btn-info btn-xs me-1' title='view' data-id='{$row->id}' data-parent-id='{$parent_id}' data-patient-id='{$row->patient->patient_id}' data-patient-name='" . str_replace('<br>', ' ', $row->patient->first_name . ' ' . $row->patient->last_name) . "' ><i class='fa-solid fa-eye'></i></a>";
                         } elseif ($row->app_date == date('Y-m-d')) {
-                            // Otherwise, show treatment icon
-                            $buttons[] = "<a href='" . route('treatment', $idEncrypted) . "' class='waves-effect waves-light btn btn-circle btn-primary btn-xs me-1' title='treatment' data-id='{$row->id}' data-parent-id='{$parent_id}' data-patient-id='{$row->patient->patient_id}' data-patient-name='" . str_replace('<br>', ' ', $row->patient->first_name . ' ' . $row->patient->last_name) . "' ><i class='fa-solid fa-stethoscope'></i></a>";
+                            $bills = PatientTreatmentBilling::where('appointment_id', $row->id)->where('status', 'Y')->get();
+                    
+                            if ($bills->isEmpty()) {
+                                $buttons[] = "<a href='" . route('treatment', $idEncrypted) . "' class='waves-effect waves-light btn btn-circle btn-primary btn-xs me-1' title='treatment' data-id='{$row->id}' data-parent-id='{$parent_id}' data-patient-id='{$row->patient->patient_id}' data-patient-name='" . str_replace('<br>', ' ', $row->patient->first_name . ' ' . $row->patient->last_name) . "' ><i class='fa-solid fa-stethoscope'></i></a>";
+                            } else {
+                                $buttons[] = "<a href='" . route('treatment', $idEncrypted) . "' class='waves-effect waves-light btn btn-circle btn-info btn-xs me-1' title='view' data-id='{$row->id}' data-parent-id='{$parent_id}' data-patient-id='{$row->patient->patient_id}' data-patient-name='" . str_replace('<br>', ' ', $row->patient->first_name . ' ' . $row->patient->last_name) . "' ><i class='fa-solid fa-eye'></i></a>";
+                            }
                         } else {
                             $buttons[] = '';
                         }
