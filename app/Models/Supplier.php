ce722d020949e3a4f264ee91b7adc6fd919d9e04
Sreelakshmi M S <sreelakshmi.ms@gmail.com>
2024-09-20 13:53:47 +0530

    Supplier table and model
    Supplier table and model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class Supplier extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $filable = ['name', 'phone', 'address', 'created_by', 'updated_by'];
+    protected $dates = ['deleted_at'];
+    protected static function booted()
+    {
+        // Before creating a new record
+        static::creating(function ($supplier) {
+            $supplier->created_by = Auth::id(); // Set created_by to current user's ID
+            $supplier->updated_by = Auth::id();
+        });
+
+        // Before updating an existing record
+        static::updating(function ($supplier) {
+            $supplier->updated_by = Auth::id(); // Set updated_by to current user's ID
+        });
+    }
+}
---
+++ database/migrations/2024_09_20_134941_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('status')->default('Y');
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
